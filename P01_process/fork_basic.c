8cd2efafcee597e273be6891d6079e78706796dd
Pradeep Tewani <pradeep@sysplay.in>
2020-06-09 05:49:21 +0530

    Added the assignments for Process

---
+++ P01_process/fork_basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    // TODO 1: Print the process id for this process
+    // TODO 2: Create the new process with fork()
+    // TODO 3: Print the pid and ppid for both the processes and child pid for parent
+
+    return 0;
+}
---
+++ P01_process/fork_execv.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main()
+{
+    // TODO 1: Declare the argument list for "ls -l /"
+    // TODO 2: Create the process with fork
+    // TODO 3: Invoke execv in the child process with above argument list
+
+    return 0;
+}
